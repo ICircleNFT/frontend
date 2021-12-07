60c30281b68bbdf57974f346d0dabcfc6f9ac239
jedi <amajidsinar@gmail.com>
2021-12-07 20:53:11 +0700

    change order

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -41,12 +41,12 @@ export default function Arts() {
                                 <div className={`mb-3 mb-sm-5 mx-auto mx-md-0 ${style.pagination}`}>
                                     <Row>
                                         <Col>
-                                            <Button className={style.coba}>PAGE</Button>
-                                        </Col>
-                                        <Col>
                                             <Link href={{ pathname: '/' }}>
                                             <Button className={style.coba}>HOME</Button>
                                             </Link>
+                                        </Col>
+                                        <Col>
+                                            <Button className={style.coba}>PAGE</Button>
                                         </Col>
                                         <Col>
                                             <Link href='/arts'>
