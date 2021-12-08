711240f393c88bca5bb86f02811a9c6a3f551a34
jedi <amajidsinar@gmail.com>
2021-12-08 22:50:17 +0700

    change layout

--- pages/arts.js
+++ pages/arts.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // reactstrap components
 import { Container, Row, Col } from "reactstrap";
-import style from "../Styles/Home.module.css"
+import style from "../Styles/Arts.module.css"
 import Link from 'next/link'
 import styled from "styled-components";
 
